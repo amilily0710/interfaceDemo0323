7873f45232f2d8d6f99bead3f7eba33f4ea600b9
amili <amilily0710@naver.com>
2021-03-23 11:17:18 +0900

    Initial commit

---
+++ standardJavaPrograms/src/main/java/com/example/standardjavaprograms/MyFirststandardJavaProg.java
@@ -0,0 +1,8 @@
+package com.example.standardjavaprograms;
+
+public class MyFirststandardJavaProg {
+    public static void main(String[] args) {
+        String hi = "Hello, my first std.Java Programs";
+        System.out.println(hi);
+    }
+}
